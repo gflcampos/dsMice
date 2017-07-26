ed3e995ae54219e6c779fd94af537ea863c527d3
gfcg <goncalo.c.goncalves@inesctec.pt>
2017-07-26 15:45:35 +0100

    1st commit

---
+++ R/meanDS.R
@@ -0,0 +1,24 @@
+#'
+#' @title Computes statistical mean of a vector
+#' @description Calculates the mean value.
+#' @details if the length of input vector is less than the set filter
+#' a missing value is returned.
+#' @param xvect a vector
+#' @return a numeric, the statistical mean
+#' @author Gaye, A.
+#' @export
+#'
+meanDS <- function (xvect) {
+  
+  # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
+  check <- isValidDS(xvect)
+  
+  # return missing value if the input vector is not valid
+  if(check){
+    result <- mean(xvect, na.rm=TRUE)
+  }else{
+    result <- NA
+  }
+  
+  return(result)
+}
