dbdb30d20fe3cff71e349c7044ea34d501b4cf0a
gfcg <goncalo.c.goncalves@inesctec.pt>
2017-08-08 11:15:17 +0100

    added cc

--- R/pmmDS.R
+++ R/pmmDS.R
@@ -1,12 +1,38 @@
 #'
-#' @title Computes statistical mean of a vector
-#' @description Calculates the mean value.
-#' @details if the length of input vector is less than the set filter
-#' a missing value is returned.
-#' @param xvect a vector
-#' @return a numeric, the statistical mean
-#' @author Gaye, A.
-#' @export
+#'@title placeholder
+#'@description placeholder
+#'@param y Vector to be imputed
+#'@param ry Logical vector of length \code{length(y)} indicating the 
+#'the subset \code{y[ry]} of elements in \code{y} to which the imputation 
+#'model is fitted. The \code{ry} generally distinguishes the observed 
+#'(\code{TRUE}) and missing values (\code{FALSE}) in \code{y}.
+#'@param x Numeric design matrix with \code{length(y)} rows with predictors for 
+#'\code{y}. Matrix \code{x} may have no missing values.
+#'@param wy Logical vector of length \code{length(y)}. A \code{TRUE} value 
+#'indicates locations in \code{y} for which imputations are created.
+#'@param donors The size of the donor pool among which a draw is made. 
+#'The default is \code{donors = 5L}. Setting \code{donors = 1L} always selects 
+#'the closest match, but is not recommended. Values between 3L and 10L 
+#'provide the best results in most cases (Morris et al, 2015).
+#'@param matchtype Type of matching distance. The default choice 
+#'(\code{matchtype = 1L}) calculates the distance between 
+#'the \emph{predicted} value of \code{yobs} and 
+#'the \emph{drawn} values of \code{ymis} (called type-1 matching). 
+#'Other choices are \code{matchtype = 0L} 
+#'(distance between predicted values) and \code{matchtype = 2L} 
+#'(distance between drawn values).
+#'@param ridge The ridge penalty used in \code{.norm.draw()} to prevent 
+#'problems with multicollinearity. The default is \code{ridge = 1e-05}, 
+#'which means that 0.01 percent of the diagonal is added to the cross-product. 
+#'Larger ridges may result in more biased estimates. For highly noisy data 
+#'(e.g. many junk variables), set \code{ridge = 1e-06} or even lower to 
+#'reduce bias. For highly collinear data, set \code{ridge = 1e-04} or higher.
+#'@param \dots Other named arguments.
+#'@return Vector with imputed data, same type as \code{y}, and of length 
+#'\code{sum(wy)}
+#'@author Stef van Buuren, Karin Groothuis-Oudshoorn
+#'@return a numeric, the statistical mean
+#'@export
 #'
 pmmDS <- function (y, ry, x, wy = NULL, donors = 5, 
                    matchtype = 1L, ridge = 1e-05, ...)
