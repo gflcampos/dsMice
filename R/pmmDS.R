bc90ab7f9cbc24697590298a2c89146a32c3702a
gfcg <goncalo.c.goncalves@inesctec.pt>
2017-08-11 18:24:05 +0100

    add chaeck to pmmDS

--- R/pmmDS.R
+++ R/pmmDS.R
@@ -67,12 +67,11 @@
 pmmDS <- function (y, ry, x, wy = NULL, donors = 5, 
                    matchtype = 1L, ridge = 1e-05, ...) {
   # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
-  #check <- isValidDS(xvect)
-  check <- TRUE
+  check <- dsBase:::isValidDS(y)
   
-  if(check) { # disable datashield check for now
+  if(check) {
     result <- mice::mice.impute.pmm(y, ry, x, wy=wy, donors=donors, matchtype=matchtype, ridge=ridge)
-  } else { # return NA if the input vector is not valid
+  } else { # return NA if the input is not valid
     result <- NA
   }
   
